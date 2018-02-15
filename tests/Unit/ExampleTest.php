3895e7cf825ea6cfad1fac0692b1952070e372cd
stay <mestayinloves@gmail.com>
2018-02-15 18:25:40 +0800

    Basic support to travis ci

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,5 +15,16 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->post(route('login'), [
+            'username' => 'laravel',
+            'password' => 'password',
+            '_token' => csrf_token()
+        ]);
+        $response->assertStatus(302);
+        $response->assertSee('laravel');
     }
 }
