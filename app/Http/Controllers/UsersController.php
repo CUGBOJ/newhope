da09b13c15d95378b36e4a580c8c5b683ce052d7
stay <mestayinloves@gmail.com>
2018-02-20 15:11:51 +0800

    Enable AuthenticateSession middleware

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -67,6 +67,9 @@ class UsersController extends Controller
         }
         $user->update($data);
 
+        //Update password_hash in the Session
+        Auth::login($user);
+
         session()->flash('success', '个人资料更新成功！');
 
         return redirect()->route('users.show', $user->username);
