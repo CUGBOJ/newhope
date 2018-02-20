96239668bff7a7356d6cb049ca2c3723f49466a4
stay <mestayinloves@gmail.com>
2018-02-20 10:49:41 +0800

    Support UI for registration

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -11,38 +11,35 @@
 
 @section('content')
     <div>
-        <h1>这是注册！！！</h1>
+        <h1>注册</h1>
         @include('share._error')
-        <form action="{{ route('users.store') }}"  method="POST" style="margin: 200px 600px">
-            {{ csrf_field() }}
-            <div>
-                <label for="username">用户名：</label>
-                <input type="text" name="username" class="form-control" value="{{ old('username') }}" >
-            </div>
-            <div>
-                <label for="nickname">昵称：</label>
-                <input type="text" name="nickname" class="form-control" value="{{ old('nickname') }}">
-            </div>
-            <div>
-                <label for="email">邮箱：</label>
-                <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-            </div>
-            <div>
-                <label for="password">密码：</label>
-                <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-            </div>
-            <div>
-                <label for="password_confirmation">确认密码：</label>
-                <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-            </div>
-            <div>
-                <label for="school">学校：</label>
-                <input type="text" name="school" class="form-control" value="{{ old('school') }}" >
-            </div>
-            <button type="submit">注册</button>
-            <button type="reset">reset</button>
-
-        </form>
+        <i-col span="6" offset="6">
+            <i-form :label-width="80" action="{{ route('users.store') }}" method="POST">
+                {{ csrf_field() }}
+                <form-item label="用户名">
+                    <i-input name="username" value="{{ old('username') }}" clearable> </i-input>
+                </form-item>
+                <form-item label="昵称">
+                    <i-input name="nickname" value="{{ old('nickname') }}" clearable> </i-input>
+                </form-item>
+                <form-item label="邮箱">
+                    <i-input name="email" type="email" value="{{ old('email') }}" clearable> </i-input>
+                </form-item>
+                <form-item label="密码">
+                    <i-input name="password" type="password" clearable> </i-input>
+                </form-item>
+                <form-item label="确认密码">
+                    <i-input name="password_confirmation" type="password" clearable></i-input>
+                </form-item>
+                <form-item label="学校">
+                    <i-input name="school" value="{{ old('school') }}" clearable></i-input>
+                </form-item>
+                <button-group style="float: right">
+                    <i-button type="primary" html-type="submit">注册</i-button>
+                    <i-button type="warning" html-type="reset">重置</i-button>
+                </button-group>
+            </i-form>
+        </i-col>
     </div>
 @stop
 
