3c0fd19afef33ca9826c04fa99649ed6b03f9fb9
stay <mestayinloves@gmail.com>
2018-02-22 21:44:54 +0800

    Support Login API

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -12,6 +12,8 @@ class SessionsController extends Controller
     {
         return view('sessions.create');
     }
+
+
     public function store(Request $request)
     {
         $credentials = $this->validate($request, [
@@ -19,23 +21,37 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
-            $user = Auth::user();
-            $user->last_login_ip = $request->ip();
-            $user->save();
-            session()->flash('success', '登陆成功');
-            return redirect()->intended(route('users.show', [Auth::user()]));
+        if ($request->expectsJson()) {
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $user->last_login_ip = $request->ip();
+                $user->save();
+                return response()->json(['message' => "login successfully"], 200);
+            } else {
+                return response()->json(['message' => "login failed"], 401);
+            }
+
         } else {
-            session()->flash('danger', '很抱歉，您的用户名和密码不匹配');
-            return redirect()->back();
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $user->last_login_ip = $request->ip();
+                $user->save();
+                session()->flash('success', '登陆成功');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                session()->flash('danger', '很抱歉，您的用户名和密码不匹配');
+                return redirect()->back();
+            }
         }
     }
+
     public function destroy()
     {
         Auth::logout();
         session()->flash('success', '您已成功退出！');
         return redirect('login');
     }
+
     public function __construct()
     {
         $this->middleware('guest', [
