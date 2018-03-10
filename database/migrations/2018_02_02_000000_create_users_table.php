6c63ce5faf620db54fe0a5147d4cf9bb6d87f305
STay <mestayinloves@gmail.com>
2018-03-10 15:33:52 +0800

    Revise table schema - fix bug in contests' title - restrict username length - set schema defaultStringLength - remove password_reset table

--- database/migrations/2018_02_02_000000_create_users_table.php
+++ database/migrations/2018_02_02_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->integer('id',1);
             $table->string('nickname');
-            $table->string('username')->unique();
+            $table->string('username',50)->unique();
             $table->string('email');
             $table->string('password');
             $table->string('school');
--- database/migrations/2018_03_04_201242_create_contests_table.php
+++ database/migrations/2018_03_04_201242_create_contests_table.php
@@ -22,7 +22,7 @@ class CreateContestsTable extends Migration
 
             $table->string('owner',255);
             $table->string('password',2048)->nullable();
-            $table->string('title',255)->unique();
+            $table->string('title',255);
 
             $table->longText('description')->nullable();
             $table->boolean('isprivate')->default(0);//if private only user in table contest_user can view.
