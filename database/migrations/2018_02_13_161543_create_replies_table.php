0acbc2de4c0b9715f8385f71d1b523b6d0d7670f
wangdehu <961337146@qq.com>
2018-03-23 23:08:48 +0800

    revise schema (#27)

--- database/migrations/2018_02_13_161543_create_replies_table.php
+++ database/migrations/2018_02_13_161543_create_replies_table.php
@@ -3,21 +3,21 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRepliesTable extends Migration 
+class CreateRepliesTable extends Migration
 {
-	public function up()
-	{
-		Schema::create('replies', function(Blueprint $table) {
-            $table->integer('id',1);
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->integer('id', 1);
             $table->integer('topic_id')->index();
             $table->string('username')->index();
             $table->text('content');
             $table->timestamps();
         });
-	}
+    }
 
-	public function down()
-	{
-		Schema::drop('replies');
-	}
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
 }
--- database/seeds/StatusesTableSeeder.php
+++ database/seeds/StatusesTableSeeder.php
@@ -9,24 +9,19 @@ class StatusesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
     public function run()
     {
-        //
         $user_names = User::all()->pluck('username')->toArray();
-        $pids=Problem::all()->pluck('id')->toArray();
+        $pids = Problem::all()->pluck('id')->toArray();
         $faker = app(Faker\Generator::class);
 
         $statuses = factory(Status::class)
             ->times(1000)
             ->make()
-            ->each(function ($status, $index)
-            use ($user_names, $pids, $faker)
-            {
+            ->each(function ($status, $index) use ($user_names, $pids, $faker) {
                 // 从用户名 数组中随机取出一个并赋值
-                $status->Username= $faker->randomElement($user_names);
+                $status->username = $faker->randomElement($user_names);
 
                 // 问题 ID，同上
                 $status->pid = $faker->randomElement($pids);
@@ -34,6 +29,5 @@ class StatusesTableSeeder extends Seeder
 
         // 将数据集合转换为数组，并插入到数据库中
         Status::insert($statuses->toArray());
-
     }
 }
