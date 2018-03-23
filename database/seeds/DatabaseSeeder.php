1da86b300fd6e2a0c2abe0be6139ae3277765da3
wangdehu <961337146@qq.com>
2018-03-23 22:32:56 +0800

    change databaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -7,17 +7,18 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-
     public function run()
     {
         Model::unguard();
         $this->call([
             PermissionsTableSeeder::class,
             RolesTableSeeder::class,
+            ProblemsTableSeeder::class,
             UsersTableSeeder::class,
+            StatusesTableSeeder::class,
+            TopicsTableSeeder::class,
+            ReplysTableSeeder::class,
             AnnouncementsTableSeeder::class,
             ContestsTableSeeder::class,
         ]);
