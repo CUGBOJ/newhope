6fd50d0c7bbed1811e4de72d212e0b2fa9e61ff7
wangdehu <961337146@qq.com>
2018-03-26 00:14:30 +0800

    Support virtual judge for CUGB (#29)

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Auth;
+use App\Handlers\PostCodeToCugbOj;
 use App\Models\Status;
+use Auth;
 use Illuminate\Http\Request;
 
 class StatusesController extends Controller
@@ -21,7 +22,7 @@ class StatusesController extends Controller
         $status = Status::getModel();
 
         if ($request->get('search')) {
-            $search = '%'.$request->get('search').'%';
+            $search = '%' . $request->get('search') . '%';
             $status = $status->orWhere('id', 'like', $search);
             $status = $status->orWhere('username', 'like', $search);
             $status = $status->orWhere('pid', 'like', $search);
@@ -52,25 +53,62 @@ class StatusesController extends Controller
         return $status->orderByDesc('id')->paginate($perPage, ['*'], 'page', $page);
     }
 
-    public function store(Request $request)
+    public function store(Request $request, PostCodeToCugbOj $post)
     {
+
+        $lang_change = array(
+            0, 2, 1, 3, 6, 5, 16, 0, 9, 7,
+        );
+        $lang = $lang_change[$request->lang];
+        if ($lang == 0) {
+            return response()->json(['message' => 'fail submit'], 200);
+        }
+        $data = array(
+            'user_id' => 'virtual_judger',
+            'problem_id' => $request->pid + 999,
+            'language' => $lang,
+            'isshare' => 0,
+            'source' => $request->code,
+            'login' => 'Submit',
+        );
+        $url = "http://acm.cugb.edu.cn/ajax/problem_submit.php";
+        $res = $post->post($url, $data);
+
+        $result_change = array(
+            'Accepted' => 1,
+            'Wrong Answer' => 2,
+            'Presentation Error' => 3,
+            'Time Limit Exceed' => 4,
+            'Runtime Error' => 5,
+            'Memory Limit Exceed' => 6,
+            'Output limit' => 7,
+            'Judge Error' => 8,
+            'Compile Error' => 9,
+            'Restricted Function' => 10,
+        );
+        $result = json_decode(substr($res, 3))->info;
         $status = Status::create([
             'username' => Auth::user()->username,
             'pid' => $request->pid,
-            'result' => 12,
+            'result' => $result_change[$result[0]],
             'lang' => $request->lang,
             'submit_time' => now(),
             'code' => $request->code,
             'length' => strlen($request->code),
+            'time' => $result[1],
+            'memory' => $result[2],
         ]);
 
-        return response()->json(['message' => 'successful submit'], 200);
+        return response()->json(['message' => 'successful submit',
+            'info' => $res,
+            'other' => $result,
+        ], 200);
     }
 
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show',  'index'],
+            'except' => ['show', 'index'],
         ]);
     }
 }
