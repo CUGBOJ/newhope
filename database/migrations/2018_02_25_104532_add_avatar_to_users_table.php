d6c44cb856aa2f882c20e60c0a778bb1e802c1b5
wangdehu <961337146@qq.com>
2018-03-26 19:57:59 +0800

    add default folder and change default avatar (#37)
    * add default folder and change  default avatar

--- database/migrations/2018_02_25_104532_add_avatar_to_users_table.php
+++ database/migrations/2018_02_25_104532_add_avatar_to_users_table.php
@@ -14,7 +14,7 @@ class AddAvatarToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable()->default('/uploads/images/avatars/201701/01/default.jpg');
+            $table->string('avatar')->nullable()->default('/uploads/default/default_avatar.jpg');
         });
     }
 
