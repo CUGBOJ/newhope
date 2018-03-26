8ae38120f6e85fa1093d47bd0757b183d19d2d87
wangdehu <961337146@qq.com>
2018-03-26 14:23:06 +0800

    fix bug of avatar

--- database/migrations/2018_02_25_104532_add_avatar_to_users_table.php
+++ database/migrations/2018_02_25_104532_add_avatar_to_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddAvatarToUsersTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddAvatarToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable()->default(config('app.url').'/uploads/images/avatars/201701/01/default.jpg');
+            $table->string('avatar')->nullable()->default('/uploads/images/avatars/201701/01/default.jpg');
         });
     }
 
