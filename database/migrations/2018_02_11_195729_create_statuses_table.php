0a266d7b5ca0dbe45ddcab97f5cd08e19cbb4de3
wangdehu <961337146@qq.com>
2018-03-26 15:21:56 +0800

    add ce_info

--- database/migrations/2018_02_11_195729_create_statuses_table.php
+++ database/migrations/2018_02_11_195729_create_statuses_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateStatusesTable extends Migration
 {
@@ -24,6 +24,7 @@ class CreateStatusesTable extends Migration
             $table->timestamp('submit_time');
             $table->text('code');
             $table->boolean('be_judged')->default(false);
+            $table->text('ce_info')->nullable();
         });
     }
 
