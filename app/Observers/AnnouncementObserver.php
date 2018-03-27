be0590a8942cc4dcad19600824d806909faedce2
wangdehu <961337146@qq.com>
2018-03-27 13:52:29 +0800

    add StatusObserver of created (#39)

--- app/Observers/AnnouncementObserver.php
+++ app/Observers/AnnouncementObserver.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Observers;
 
-use App\Notifications\AnnouncementReplied;
 use App\Models\Announcement;
 use App\Models\User;
+use App\Notifications\AnnouncementReplied;
 
 class AnnouncementObserver
 {
     public function created(Announcement $announcement)
     {
-        $users=User::all();
-        foreach($users as $user){
+        $users = User::all();
+        foreach ($users as $user) {
             $user->messages(new AnnouncementReplied($announcement));
             $user->save();
         }
