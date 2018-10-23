ab7de3456ba5fe8817e1db0beb4e2b2765807b0a
Sanxing Chen <mestayinloves@gmail.com>
2018-10-23 21:13:40 +0800

    Delete useless views files (#52)

--- resources/views/contests/_contest.blade.php
+++
@@ -1,8 +0,0 @@
-<li>
-    <a href="{{ route('contests.add_user_by_password', $contest->id )}}" >
-
-        {{$contest->id}}_{{ $contest->title }}_{{$contest->start_time}}_{{$contest->endtime}}_{{$contest->isprivate}}
-
-
-    </a>
-</li>
