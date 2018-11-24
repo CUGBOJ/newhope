efb52361cb45890b62ed745570a88b2f164dc0a1
wangdehu <961337146@qq.com>
2018-11-24 20:00:06 +0800

    Fix  error

--- database/migrations/2018_11_24_134923_create_teams_table.php
+++ database/migrations/2018_11_24_134923_create_teams_table.php
@@ -16,7 +16,7 @@ class CreateTeamsTable extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->integer('id', 1)->unsigned();
             $table->string('teamname', 50)->unique();
-            $table->intefer('member_number')->default(0);
+            $table->integer('member_number')->default(0);
             $table->integer('contest_id');
             $table->timestamps();
         });
